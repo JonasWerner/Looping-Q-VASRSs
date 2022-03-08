9154e68e2b5a451b2f1c29ddc68dafa30b659c26
Daniel Dietsch <daniel.dietsch@gmail.com>
2022-03-08 10:30:32 +0100

    fix latex build errors with old --src-special command, remove duplicate bibtex, other small things

--- thesis/build-main.cmd
+++ thesis/build-main.cmd
@@ -1,2 +1,1 @@
-latexmk -pvc -pdf -pdflatex="pdflatex -shell-escape -synctex=1 -interaction=nonstopmode --src-specials" -aux-directory=./main-tmp qVasrThesis.tex
-pause
+latexmk -pvc -pdf -pdflatex="pdflatex -shell-escape -synctex=1 -interaction=nonstopmode" -aux-directory=./main-tmp qVasrThesis.tex
